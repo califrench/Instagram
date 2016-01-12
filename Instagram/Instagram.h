48d6448347f961607211236302d47eab8bfd0d36
Scott Gauthreaux <me@scottgauthreaux.com>
2016-01-12 23:41:09 +0100

    Initial Commit

---
+++ Instagram/Instagram.h
@@ -0,0 +1,19 @@
+//
+//  Instagram.h
+//  Instagram
+//
+//  Created by Scott Gauthreaux on 12/01/16.
+//  Copyright © 2016 Scott Gauthreaux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Instagram.
+FOUNDATION_EXPORT double InstagramVersionNumber;
+
+//! Project version string for Instagram.
+FOUNDATION_EXPORT const unsigned char InstagramVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Instagram/PublicHeader.h>
+
+
